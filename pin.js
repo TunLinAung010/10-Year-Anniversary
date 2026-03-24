3683931b5e092a3cbe6495367ed6e4d567a4ea4a
Tun Lin Aung <75923146+TunLinAung010@users.noreply.github.com>
2026-03-24 22:35:22 +0630

    Add files via upload

---
+++ pin.js
@@ -0,0 +1,15 @@
+// Set your default PIN
+const correctPin = "112016";  
+
+// Wait for button click
+document.getElementById("submit-pin").addEventListener("click", () => {
+  const entered = document.getElementById("pin-input").value;
+
+  if (entered === correctPin) {
+    // PIN correct → redirect to main.html
+    window.location.href = "main.html";  
+  } else {
+    // PIN incorrect → show error message
+    document.getElementById("error-msg").textContent = "Incorrect PIN!";
+  }
+});
